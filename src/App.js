d5a74b8152dbf6c9e63bbf881f8958ef06ad0e3d
Flor2801 <florenciasarria1@hotmail.com>
2021-05-27 11:32:07 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [personajes, setPersonajes] = useState([]);
+  const [value, setValue] = useState("");
+  const [gender, setGender] = useState("");
+
+  useEffect(() => {
+    
+    fetch(`https://rickandmortyapi.com/api/character/?name=${value}&gender=${gender}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPersonajes(data.results);
+      });
+  }, [value, gender]);
+
+
+const handleChange = (e) => {
+  setValue(e.target.value)
+}
+
+const handleGender = (e) => {
+  setGender(e.target.value)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="contenedor">
+        <div>
+          FILTROS
+          <input onChange={handleChange}type="text"></input>
+          Female<input onChange={handleGender}  type="radio" value="female" name="gender"></input>
+          Male<input onChange={handleGender} type="radio" value="male" name="gender"></input>
+          Genderless<input onChange={handleGender} type="radio" value="genderless" name="gender"></input>
+          Undefined<input  onChange={handleGender} type="radio" value="undefined" name="gender"></input>
+          <button>BUSCAR</button>
+        </div>
+
+        <div className="contenido">
+          {personajes.map((personajes) => (
+            <div className="card">
+              <div className="card-image">
+                <img src={personajes.image}></img>
+              </div>
+
+              <div className="card-description">
+                {personajes.name}
+                {personajes.species}
+                {personajes.location.name}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      );
+    </>
   );
 }
 
---
+++ src/tarjeta.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+// const Tarjeta = ({imagen, name, personaje,location}) => {
+//     return(
+//     <div>
+//     <div className="card">
+//     <div className="card-image">
+//       <img src={personajes.image}></img>
+//     </div>
+//     <div className="card-description">
+//       {personajes.name}
+//       {personajes.species}
+//       {personajes.location.name}
+//     </div>
+//     <div/>
+//     )
+// }
+
+// export default Tarjeta
